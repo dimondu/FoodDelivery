395aa27b02b80fcec6e6e6e4c72575c6ff4cc68a
ddurov <dmitrijdurov@MacBook-Air-Dmitrij.local>
2024-10-21 21:48:26 +0300

    Старт проекта

---
+++ FoodDelivery/SceneDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  SceneDelegate.swift
+//  FoodDelivery
+//
+//  Created by Дмитрий Дуров on 21.10.2024.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+    }
+}
---
+++ FoodDelivery/ViewController.swift
@@ -0,0 +1,11 @@
+//
+//  ViewController.swift
+//  FoodDelivery
+//
+//  Created by Дмитрий Дуров on 21.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+}
